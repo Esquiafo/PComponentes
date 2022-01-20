6bcb317e4ca5449ec12a8cba55a4d23909efa732
Esquiafo <57331052+Esquiafo@users.noreply.github.com>
2022-01-20 02:26:34 -0300

    Cambio esteticos

--- src/elements/Navbar.js
+++ src/elements/Navbar.js
@@ -1,22 +1,55 @@
 import React from 'react';
 import {  Link } from "react-router-dom";
 import styles from '../elements/Navbar.css';
+import { Button, Icon } from 'semantic-ui-react'
 
 const navbar= () =>{
   return (
   <div className='navback'>
-    <li>
-        <a>LOGO</a>
-    </li>
-    <li>
-      <Link to="/">Index</Link>
-    </li>
-    <li>
-      <Link to="/Shop">Shop</Link>
-    </li>
-    <li>
-      <Link to="/Forum">Forum</Link>
-    </li>
+     
+
+      <Link className='link' to="/">
+      <Button animated>
+      <Button.Content visible> <Link className='link' to="/">Index</Link></Button.Content>
+      <Button.Content hidden>
+        <Icon name='long arrow alternate left' />
+        </Button.Content>
+      </Button>
+      </Link>
+
+
+
+
+      <Link className='link' to="/forum">
+      <Button animated>
+
+        <Button.Content visible> 
+         <Link className='link' to="/forum">Forum</Link>
+        </Button.Content>
+      <Button.Content hidden>
+        <Icon name='chat' />
+      </Button.Content>
+
+      </Button>
+      </Link>
+        
+
+
+
+      <Link className='link' to="/shop">
+      <Button animated>
+
+        <Button.Content visible> 
+          <Link className='link' to="/"> <a className='link'>Cart</a></Link>
+        </Button.Content>
+      <Button.Content hidden>
+        <Icon name='shop' /><a className='link'>:</a><a className='link'>N~</a>
+      </Button.Content>
+
+      </Button>
+      </Link>
+       
+
   </div>
   );
 }
