fb8528b25ffc4ed95067070cdba6bda14b524a30
Esquiafo <57331052+Esquiafo@users.noreply.github.com>
2022-03-03 20:31:32 -0300

    Path Cate

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Route exact path="/products" component={Product}/>
         <Route exact path="/category" component={Category}/>
         <Route path="/products/:productId" component={SingleProduct}/>
-        <Route path='/category/:categoryId' component={SingleCategory} />
+        <Route path="/category/:categoryId" component={SingleCategory} />
 
         <Route exact path='/cart' component={Cart} />
       </Switch>
